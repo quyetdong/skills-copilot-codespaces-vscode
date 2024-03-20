d9d78e2113187300f4b7cf2f2e39a1d3159eaa92
Dong Quyet <30405581+quyetdong@users.noreply.github.com>
2024-03-20 08:25:20 +0000

    copilot comments

---
+++ comments.js
@@ -0,0 +1,21 @@
+// create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsPath = path.join(__dirname, 'comments.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/comments', function(req, res){
+    fs.readFile(commentsPath, function(err, data){
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        res.json(JSON.parse(data));
+    });
+});
