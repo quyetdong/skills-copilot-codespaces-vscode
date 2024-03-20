861d8a20c73cdc7e5d61974e0a728edda4f65c67
Dong Quyet <30405581+quyetdong@users.noreply.github.com>
2024-03-20 08:55:52 +0000

    copilot comments

--- comments.js
+++ comments.js
@@ -1,21 +1,45 @@
 // create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var path = require('path');
-var commentsPath = path.join(__dirname, 'comments.json');
+// create a route for /comments
+// create a route for /comments/new
+// create a route for /comments/:id
+// create a route for /comments/:id/edit
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+var express = require("express");
+var router = express.Router();
 
-app.get('/comments', function (req, res) {
-    fs.readFile(commentsPath, function (err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        res.json(JSON.parse(data));
-    });
+// index route
+router.get("/", function(req, res) {
+   res.send("This is the comments index route");
 });
+
+// new route
+router.get("/new", function(req, res) {
+   res.send("This is the comments new route");
+});
+
+// create route
+router.post("/", function(req, res) {
+   res.send("This is the comments create route");
+});
+
+// show route
+router.get("/:id", function(req, res) {
+   res.send("This is the comments show route");
+});
+
+// edit route
+router.get("/:id/edit", function(req, res) {
+   res.send("This is the comments edit route");
+});
+
+// update route
+router.put("/:id", function(req, res) {
+   res.send("This is the comments update route");
+});
+
+// destroy route
+router.delete("/:id", function(req, res) {
+   res.send("This is the comments destroy route");
+});
+
+module.exports = router;
